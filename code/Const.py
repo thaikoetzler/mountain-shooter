24b9f8676246fdbc91498a224a2db275fef62a74
Thais Koetzler <thaialessandra28@gmail.com>
2026-06-24 18:16:08 -0300

    Criação de Mediator. Destruição de inimigos.

--- code/Const.py
+++ code/Const.py
@@ -6,6 +6,24 @@ COLOR_WHITE = (255, 255, 255)
 COLOR_YELLOW = (255, 255, 153)
 
 # E
+ENTITY_HEALTH = {
+    'Level1Bg0': 999,
+    'Level1Bg1': 999,
+    'Level1Bg2': 999,
+    'Level1Bg3': 999,
+    'Level1Bg4': 999,
+    'Level1Bg5': 999,
+    'Level1Bg6': 999,
+    'Player1'  : 300,
+    'Player1Shot': 1,
+    'Player2' : 300,
+    'Player2Shot': 1,
+    'Enemy1': 50,
+    'Enemy1Shot': 1,
+    'Enemy2': 60,
+    'Enemy2Shot': 1,
+}
+
 ENTITY_SPEED = {
     'Level1Bg0': 0,
     'Level1Bg1': 1,
@@ -19,6 +37,7 @@ ENTITY_SPEED = {
     'Enemy1'   : 2,
     'Enemy2'   : 1,
 }
+
 EVENT_ENEMY = pygame.USEREVENT + 1
 
 # M

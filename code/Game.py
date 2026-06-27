1820becb81f8aba076f7a4dfdd121fa32cf8f111
Thais Koetzler <thaialessandra28@gmail.com>
2026-06-27 19:36:20 -0300

    Implementação do SQLite3 para o Score com o uso de proxy

--- code/Game.py
+++ code/Game.py
@@ -3,6 +3,7 @@ import pygame
 from code.Const import WIN_WIDTH, WIN_HEIGHT, MENU_OPTION
 from code.Level import Level
 from code.Menu import Menu
+from code.Score import Score
 
 
 class Game:
@@ -12,6 +13,7 @@ class Game:
 
     def run(self):
         while True:
+            score = Score(self.window)
             menu = Menu(self.window)
             menu_return = menu.run()
 
@@ -22,7 +24,11 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
 
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()
                 quit()
